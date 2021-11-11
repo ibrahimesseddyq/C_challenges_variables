b98b73a46ba82d3f5b1322893b6d8600755e59d9
armanovxx <iebeat2018@gmail.com>
2021-11-11 10:13:51 +0100

    revision c10

--- Challenge_10.c
+++ Challenge_10.c
@@ -2,10 +2,10 @@
 int main()
 {
     float conf;
-    int rayon;
-    int pi=3.14;
+    float rayon;
+    float pi=3.14;
     printf("entrer le rayon : ");
-    scanf("%d",&rayon);
+    scanf("%f",&rayon);
     conf =2*pi*rayon;
     printf("resultat est %f",conf);
     
